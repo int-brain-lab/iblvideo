b47831d47476675e0e550e0cf4f79c73d8a38e3f
mschart <michael.m.schartner@gmail.com>
2018-09-11 16:49:09 -0500

    Update MasterVideo.py

--- MasterVideo.py
+++ MasterVideo.py
@@ -11,5 +11,5 @@ numCams = ConfigureCameras()
 # Start subprocesses for each camera
 print('Cameras configured, starting %s cameras'%numCams)
 for i in range(numCams):
-    Popen(['gnome-terminal', '-e', 'python /home/vid/iblvideo/CaptureCamera.py ' + str(i)])
+    Popen(['gnome-terminal', '-e', 'python CaptureCamera.py ' + str(i)])
 
