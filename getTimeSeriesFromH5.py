0cc7b83b0b55897ce07adb1205ff19b8833f548a
mschart <michael.m.schartner@gmail.com>
2019-03-19 12:02:55 +0100

    Update getTimeSeriesFromH5.py

--- getTimeSeriesFromH5.py
+++ getTimeSeriesFromH5.py
@@ -2,6 +2,10 @@ import pandas
 
 df=pandas.read_hdf('pupilDeepCut_resnet50_pupilsep20shuffle1_200000.h5')
 
+#To get a list of the available points, type:
+
+df.keys()
+	
 '''
 For a particular bodypart, you can access the time series of the 'x', 'y' coordinate and detection certainty 'likelihood'
 E.g. to get the x position of the 'pupil_top_r' point, type:
