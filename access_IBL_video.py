e9f32a971a7df847e04794ac8d51a7421d3708c8
mschart <michael.m.schartner@gmail.com>
2019-04-10 18:04:51 +0200

    Update access_IBL_video.py

--- access_IBL_video.py
+++ access_IBL_video.py
@@ -1,5 +1,4 @@
 from oneibl.one import ONE
-from ibllib.misc import pprint
 
 one = ONE()
 
@@ -7,12 +6,13 @@ def get_most_recent_video_from_all_labs():
 
  '''
  this assumes you have oneibl installed and access;
- a couple recent IBL trainig-rig videos will be downloaded 
+ a couple recent IBL trainig-rig videos across labs will be downloaded
+ from flatiron server to your local ONE folder
  '''
  
  for lab in one.list(keyword='lab'):
   sessions, details = one.search(dataset_types='_iblrig_leftCamera.raw', lab=lab, 
-  details=True,date_range=['2019-03-01', '2019-04-10'])
+  details=True,date_range=['2019-03-01', '2030-04-10'])
   try:                                             
    dates=[i['end_time'] for i in details]
    for jj in range(1,5):
