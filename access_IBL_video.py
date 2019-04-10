a9974ca2b6331b0fb5d6f2176972b007c4a6de5f
mschart <michael.m.schartner@gmail.com>
2019-04-10 18:02:51 +0200

    Update access_IBL_video.py

--- access_IBL_video.py
+++ access_IBL_video.py
@@ -1,21 +1,24 @@
 from oneibl.one import ONE
-one = ONE()
 from ibllib.misc import pprint
 
+one = ONE()
 
 def get_most_recent_video_from_all_labs():
- D=[]
+
+ '''
+ this assumes you have oneibl installed and access;
+ a couple recent IBL trainig-rig videos will be downloaded 
+ '''
+ 
  for lab in one.list(keyword='lab'):
   sessions, details = one.search(dataset_types='_iblrig_leftCamera.raw', lab=lab, 
   details=True,date_range=['2019-03-01', '2019-04-10'])
   try:                                             
    dates=[i['end_time'] for i in details]
    for jj in range(1,5):
-    idx=argsort(dates)[-jj] 
-    D.append({'lab':lab,'latest session':sessions[idx],'date':dates[idx],'details':details[idx]})  
+    idx=argsort(dates)[-jj]   
     one.load(sessions[argsort(dates)[idx]],dataset_types='_iblrig_leftCamera.raw')
    print(lab,len(sessions)) 
   except:                     
    print(lab,len(sessions))  
   
- return D
