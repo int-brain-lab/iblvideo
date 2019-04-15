0ccb29781bdf81dcae2ca795a5f77f8f1048bea5
mschart <michael.m.schartner@gmail.com>
2019-04-15 16:34:34 +0200

    Update getTimeSeriesFromH5.py

--- getTimeSeriesFromH5.py
+++ getTimeSeriesFromH5.py
@@ -1,4 +1,5 @@
-import pandas   	
+import pandas   
+from pylab import*
 
 df=pandas.read_hdf('pupilDeepCut_resnet50_pupilsep20shuffle1_200000.h5')
 
@@ -17,5 +18,14 @@ s=df[(df.keys()[0][0], 'pupil_top_r', 'x')].values
 In practice it is important only to consider the coordinates where the network was certain, i.e. with a detection likelihood above 0.9
 '''
 
-	
+part=pupil_top_r',
+
+x_values=df[(df.keys()[0][0], part, 'x')].values
+y_values=df[(df.keys()[0][0], part, 'y')].values
+likelyhoods=df[(df.keys()[0][0], part, 'likelihood')].values
+
+mx = ma.masked_where(likelyhoods<0.9, x_values)
+x=ma.compressed(mx)
+my = ma.masked_where(likelyhoods<0.9, y_values)
+y=ma.compressed(my)	
 
