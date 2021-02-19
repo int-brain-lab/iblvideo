53d6eeb7f3a0fb67c182b7c4d06479775cdaba7e
juhuntenburg <ju.huntenburg@gmail.com>
2021-02-19 17:23:38 +0000

    update run script

--- production/run_with_weights.py
+++ production/run_with_weights.py
@@ -1,7 +1,7 @@
 """Script to run DLC on AWS."""
 import deeplabcut  # deeplabcut imported first thing to avoid backend error
 from oneibl.one import ONE
-from dlc_parallel import dlc_parallel
+from choiceworld import dlc
 from weights import download_weights_flatiron
 
 path_dlc = download_weights_flatiron(version_date='2021-02-15')
@@ -11,4 +11,5 @@ one = ONE()
 files_mp4 = one.load(eid, dataset_types=['_iblrig_Camera.raw'],
                      download_only=True)
 
-alf_files = dlc_parallel(files_mp4[0], path_dlc)
+alf_files = dlc(files_mp4[0], path_dlc)
+print(alf_files)
