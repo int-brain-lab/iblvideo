68c92598a36d711f5803f6d907a80407cd70c6ab
juhuntenburg <ju.huntenburg@gmail.com>
2021-02-26 16:56:57 +0000

    Deeplabcut import first

--- iblvideo/__init__.py
+++ iblvideo/__init__.py
@@ -1,4 +1,8 @@
+
 __version__ = '0.0.0'  # This is the only place where the version is hard coded, only adapt here
+
+import deeplabcut
+
 from iblvideo.run import run_session, run_queue
 from iblvideo.choiceworld import dlc, dlc_parallel
 from iblvideo.weights import download_weights
