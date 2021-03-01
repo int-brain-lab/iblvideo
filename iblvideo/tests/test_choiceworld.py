348999abe1290d2437e2ec102ced9267d1452015
juhuntenburg <ju.huntenburg@gmail.com>
2021-03-01 12:01:08 +0000

    Update test

--- iblvideo/tests/test_choiceworld.py
+++ iblvideo/tests/test_choiceworld.py
@@ -1,10 +1,10 @@
 import shutil
 import numpy as np
 import pandas as pd
-from ..choiceworld import dlc
-from ..weights import download_weights
-from .download_test_data import download_test_data
-from .. import __version__
+from iblvideo.choiceworld import dlc
+from iblvideo.weights import download_weights
+from iblvideo.tests.download_test_data import download_test_data
+from iblvideo import __version__
 
 
 def test_dlc(version=__version__):
@@ -24,8 +24,18 @@ def test_dlc(version=__version__):
         ctrl_pqt = pd.read_parquet(
             test_data.joinpath('output', f'_ibl_{cam}Camera.dlc.pqt'))
 
+        assert np.all(out_pqt.columns == ctrl_pqt.columns)
+
+        # only compare entries with likelihood over 0.9
+        targets = np.unique(['_'.join(col.split('_')[:-1]) for col in ctrl_pqt.columns])
+        for t in targets:
+            idx_ctrl = ctrl_pqt.loc[ctrl_pqt[f'{t}_likelihood'] < 0.9].index
+            idx_out = out_pqt.loc[out_pqt[f'{t}_likelihood'] < 0.9].index
+            for idx in [idx_ctrl, idx_out]:
+                ctrl_pqt.loc[idx, [f'{t}_x', f'{t}_y', f'{t}_likelihood']] = np.nan
+                out_pqt.loc[idx, [f'{t}_x', f'{t}_y', f'{t}_likelihood']] = np.nan
+
         assert np.allclose(np.array(out_pqt), np.array(ctrl_pqt), rtol=10e-2)
-        assert np.all(out_pqt.columns, ctrl_pqt.columns)
 
     alf_path = test_data.joinpath('alf')
     shutil.rmtree(alf_path)
