c0704a758b44256114df9783b827f39755f9d27a
juhuntenburg <ju.huntenburg@gmail.com>
2021-04-08 17:37:45 +0100

    Begin refactoring to enable stop and restart

--- iblvideo/run.py
+++ iblvideo/run.py
@@ -12,7 +12,7 @@ from iblvideo.weights import download_weights
 from iblvideo import __version__
 from ibllib.pipes import tasks
 from ibllib.qc.dlc import DlcQC
-from ibllib.io.video import label_from_path
+from ibllib.io.video import label_from_path, assert_valid_label
 
 _logger = logging.getLogger('ibllib')
 
@@ -25,63 +25,57 @@ class TaskDLC(tasks.Task):
     io_charge = 90
     level = 0
 
-    def _run(self, n_cams=3, version=__version__, frames=None):
+    def _run(self, cams=('left', 'body', 'right'), version=__version__, frames=None):
 
         session_id = self.one.eid_from_path(self.session_path)
-        # Check for existing dlc data
-        dlc_data = self.one.alyx.rest('datasets', 'list', django=(f'session__id,{session_id},'
-                                                                  'name__icontains,dlc'))
-        # TODO: rerun only for the cams that don't yet exist
-        run_dlc = None
-        if len(dlc_data) < n_cams:
-            run_dlc = True
-            # Download the camera data
-            self.one.load(session_id, dataset_types=['_iblrig_Camera.raw'], download_only=True)
-            # Download weights into ONE Cache directory under 'resources/DLC' if not exist
-            path_dlc = download_weights(version=version)
-            # Run dlc on all videos in raw_video_data
-            files_mp4 = list(self.session_path.joinpath('raw_video_data').glob('*.mp4'))
-            _logger.info(f'Running DLC on {len(files_mp4)} video files.')
-            for cam in range(len(files_mp4)):
-                dlc_result = dlc(files_mp4[cam], path_dlc)
-                _logger.info(dlc_result)
-        else:
-            # Download dlc data if not locally available
-            self.one.load(session_id, dataset_types='camera.dlc')
-
-        # Run DLC QC, if it passes run motion energy
-        # sort results so that left is first in case one only wants to run left
-        dlc_results = sorted(self.session_path.joinpath('alf').glob('*dlc.pqt'),
-                             key=lambda f: 'left' in str(f), reverse=True)
-        for i in range(n_cams):
-            dlc_pqt = dlc_results[i]
-            label = label_from_path(dlc_pqt)
-            qc = DlcQC(session_id, label, one=self.one, log=_logger)
+        # Loop through cams on this level
+        dlc_results, me_results = [], []
+        for cam in cams:
+            # Check for existing dlc data
+            dlc_data = self.one.alyx.rest('datasets', 'list', session=session_id,
+                                          django=f'name__icontains,{cam}Camera.dlc')
+            if len(dlc_data) == 0:
+                # Download the camera data if not available locally
+                file_mp4 = self.one.load(session_id, dataset_types=['_iblrig_Camera.raw'],
+                                         download_only=True)
+                # Download weights into ONE Cache directory under 'resources/DLC' if not exist
+                path_dlc = download_weights(version=version)
+                # Run DLC and log
+                _logger.info(f'Running DLC on {cam}Camera.')
+                try:
+                    dlc_result = dlc(file_mp4, path_dlc)  # TODO: Possibly pass logger?
+                    _logger.info(dlc_result)
+                    dlc_results.append(dlc_result)
+                except BaseException:
+                    _logger.error(f'DLC {cam}Camera failed.\n' + traceback.format_exc())
+            else:
+                # Download dlc data if not locally available
+                dlc_result = self.one.load(session_id, dataset_types='camera.dlc',
+                                           download_only=True)
+            # Run DLC QC
+            qc = DlcQC(session_id, cam, one=self.one, log=_logger)
             qc.run(update=True)
-            _logger.info(f'Computing motion energy for {label}Camera')
+            _logger.info(f'Computing motion energy for {cam}Camera')
             try:
-                me_result, _ = motion_energy(self.session_path, dlc_pqt,
-                                             frames=frames, one=self.one)
+                me_result, _ = motion_energy(self.session_path, dlc_result, frames=frames,
+                                             one=self.one)
                 _logger.info(me_result)
-                me_results = list(self.session_path.joinpath('alf').glob('*ROIMotionEnergy*.npy'))
+                me_results.append(me_result)
             except BaseException:
-                _logger.error(f'Motion energy {label}Camera failed.\n' + traceback.format_exc())
-                me_results = []
+                _logger.error(f'Motion energy {cam}Camera failed.\n' + traceback.format_exc())
 
-        if run_dlc:
-            return dlc_results, me_results
-        else:
-            return [], me_results
+        return dlc_results, me_results
 
 
-def run_session(session_id, machine=None, n_cams=3, one=None, version=__version__,
-                remove_videos=True, frames=50000, **kwargs):
+def run_session(session_id, machine=None, cams=('left', 'body', 'right'), one=None,
+                version=__version__, remove_videos=True, frames=50000, **kwargs):
     """
     Run DLC on a single session in the database.
 
     :param session_id: Alyx eID of session to run
     :param machine: Tag for the machine this job ran on (string)
-    :param n_cams: Minimum number of camera datasets required
+    :param cams: Tuple of labels of videos to run dlc and motion energy on.
+                 Valid labels are 'left', 'body' and 'right'.
     :param one: ONE instance to use for query (optional)
     :param version: Version of iblvideo / DLC weights to use (default is current version)
     :param remove_videos: Whether to remove the local raw_video_data after DLC (default is True)
@@ -108,20 +102,26 @@ def run_session(session_id, machine=None, n_cams=3, one=None, version=__version_
         print(f"No DLC task found for session {session_id}")
 
     try:
-        # Check if required number of cameras is available
-        dsets = one.alyx.rest('datasets', 'list', django=(f'session__id,{session_id},'
-                                                          'data_format__name,mp4,'
-                                                          'name__icontains,camera'))
-        if len(dsets) < n_cams:
+        # Check if labels are valid
+        cams = tuple(assert_valid_label(cam) for cam in cams)  # raises ValueError if label invalid
+        # Check if all requested videos exist
+        vids = [dset['name'] for dset in one.alyx.rest('datasets', 'list',
+                                                       django=(f'session__id,{session_id},'
+                                                               'data_format__name,mp4,'
+                                                               f'name__icontains,camera')
+                                                       )]
+        no_vid = [cam for cam in cams if f'_iblrig_{cam}Camera.raw.mp4' not in vids]
+        if len(no_vid) > 0:
             # If less datasets, update task and raise error
-            patch_data = {'log': f"Found only {len(dsets)} video files, user required {n_cams}.",
-                          'version': version, 'status': 'Errored'}
+            log_str = '\n'.join([f"No raw video file found for {no_cam}Camera." for
+                                 no_cam in no_vid])
+            patch_data = {'log': log_str, 'version': version, 'status': 'Errored'}
             one.alyx.rest('tasks', 'partial_update', id=tdict['id'], data=patch_data)
             status = -1
         else:
             # create the task instance and run it, update task
             task = TaskDLC(session_path, one=one, taskid=tdict['id'], machine=machine, **kwargs)
-            status = task.run(n_cams=n_cams, version=version, frames=frames)
+            status = task.run(cams=cams, version=version, frames=frames)
             patch_data = {'time_elapsed_secs': task.time_elapsed_secs, 'log': task.log,
                           'version': version, 'status': 'Errored' if status == -1 else 'Complete'}
             one.alyx.rest('tasks', 'partial_update', id=tdict['id'], data=patch_data)
@@ -130,7 +130,6 @@ def run_session(session_id, machine=None, n_cams=3, one=None, version=__version_
                 # it is safer to instantiate the FTP right before transfer to prevent time-out
                 ftp_patcher = FTPPatcher(one=one)
                 ftp_patcher.create_dataset(path=task.outputs[0] + task.outputs[1])
-
             if remove_videos is True:
                 shutil.rmtree(session_path.joinpath('raw_video_data'), ignore_errors=True)
 
