cb8e66015cbad9fcdaa3d1acb488cedf613889ca
juhuntenburg <ju.huntenburg@gmail.com>
2021-04-19 09:28:16 +0100

    New release 1.0.0
    Weights are the same as v0.2.0

--- iblvideo/__init__.py
+++ iblvideo/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = '0.2.0'  # This is the only place where the version is hard coded, only adapt here
+__version__ = '1.0.0'  # This is the only place where the version is hard coded, only adapt here
 
 import deeplabcut
 
