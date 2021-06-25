fa8d0fb648eebc371f5c3b5c79a5fb1a984b3d43
mschart <michael.m.schartner@gmail.com>
2021-06-25 13:17:48 +0100

    Create pupil_diameter_pipeline.py

---
+++ prototyping/pupil_diameter_pipeline.py
@@ -0,0 +1,203 @@
+import numpy as np
+import alf.io
+from oneibl.one import ONE
+from scipy.interpolate import interp1d
+
+def get_dlc_XYs(eid, video_type):
+
+    #video_type = 'left'    
+    one = ONE() 
+    dataset_types = ['camera.dlc', 'camera.times']                     
+    a = one.list(eid,'dataset-types')
+    b = [x['dataset_type'] for x in a]
+    if not all([u in b for u in dataset_types]):
+        print('not all data available')    
+        return
+        
+    # clobber=True for force download            
+    one.load(eid, dataset_types = dataset_types)  
+    local_path = one.path_from_eid(eid)  
+    alf_path = local_path / 'alf'   
+    
+    cam0 = alf.io.load_object(
+        alf_path,
+        '%sCamera' %
+        video_type,
+        namespace='ibl')
+
+    Times = cam0['times']
+    cam = cam0['dlc']
+    points = np.unique(['_'.join(x.split('_')[:-1]) for x in cam.keys()])
+
+    # Set values to nan if likelyhood is too low # for pqt: .to_numpy()
+    XYs = {}
+    for point in points:
+        x = np.ma.masked_where(
+            cam[point + '_likelihood'] < 0.9, cam[point + '_x'])
+        x = x.filled(np.nan)
+        y = np.ma.masked_where(
+            cam[point + '_likelihood'] < 0.9, cam[point + '_y'])
+        y = y.filled(np.nan)
+        XYs[point] = np.array(
+            [x, y])    
+
+    return Times, XYs      
+
+
+def smooth_pupil_diameter(diam, window=31, order=3, interp_kind='cubic'):
+    '''
+    Author: Matt Whiteway
+    '''
+    signal_noisy_w_nans = np.copy(diam)
+    timestamps = np.arange(signal_noisy_w_nans.shape[0])
+    good_idxs = np.where(~np.isnan(signal_noisy_w_nans))
+    # perform savitzky-golay filtering on non-nan points
+    signal_smooth_nonans = non_uniform_savgol(
+        timestamps[good_idxs], signal_noisy_w_nans[good_idxs], window=window, polynom=order)
+    signal_smooth_w_nans = np.copy(signal_noisy_w_nans)
+    signal_smooth_w_nans[good_idxs] = signal_smooth_nonans
+    # interpolate nan points
+    interpolater = interp1d(
+        timestamps[good_idxs], signal_smooth_nonans, kind=interp_kind, fill_value='extrapolate')
+    diam = interpolater(timestamps)
+    return diam
+
+
+def non_uniform_savgol(x, y, window, polynom):
+    """
+    Applies a Savitzky-Golay filter to y with non-uniform spacing
+    as defined in x
+    This is based on https://dsp.stackexchange.com/questions/1676/savitzky-golay-smoothing-filter-for-not-equally-spaced-data
+    The borders are interpolated like scipy.signal.savgol_filter would do
+    https://dsp.stackexchange.com/a/64313
+    Parameters
+    ----------
+    x : array_like
+        List of floats representing the x values of the data
+    y : array_like
+        List of floats representing the y values. Must have same length
+        as x
+    window : int (odd)
+        Window length of datapoints. Must be odd and smaller than x
+    polynom : int
+        The order of polynom used. Must be smaller than the window size
+    Returns
+    -------
+    np.array of float
+        The smoothed y values    
+    -------    
+    Author: Matt Whiteway
+    """
+    if len(x) != len(y):
+        raise ValueError('"x" and "y" must be of the same size')
+    if len(x) < window:
+        raise ValueError('The data size must be larger than the window size')
+    if type(window) is not int:
+        raise TypeError('"window" must be an integer')
+    if window % 2 == 0:
+        raise ValueError('The "window" must be an odd integer')
+    if type(polynom) is not int:
+        raise TypeError('"polynom" must be an integer')
+    if polynom >= window:
+        raise ValueError('"polynom" must be less than "window"')
+    half_window = window // 2
+    polynom += 1
+    # Initialize variables
+    A = np.empty((window, polynom))  # Matrix
+    tA = np.empty((polynom, window))  # Transposed matrix
+    t = np.empty(window)  # Local x variables
+    y_smoothed = np.full(len(y), np.nan)
+    # Start smoothing
+    for i in range(half_window, len(x) - half_window, 1):
+        # Center a window of x values on x[i]
+        for j in range(0, window, 1):
+            t[j] = x[i + j - half_window] - x[i]
+        # Create the initial matrix A and its transposed form tA
+        for j in range(0, window, 1):
+            r = 1.0
+            for k in range(0, polynom, 1):
+                A[j, k] = r
+                tA[k, j] = r
+                r *= t[j]
+        # Multiply the two matrices
+        tAA = np.matmul(tA, A)
+        # Invert the product of the matrices
+        tAA = np.linalg.inv(tAA)
+        # Calculate the pseudoinverse of the design matrix
+        coeffs = np.matmul(tAA, tA)
+        # Calculate c0 which is also the y value for y[i]
+        y_smoothed[i] = 0
+        for j in range(0, window, 1):
+            y_smoothed[i] += coeffs[0, j] * y[i + j - half_window]
+        # If at the end or beginning, store all coefficients for the polynom
+        if i == half_window:
+            first_coeffs = np.zeros(polynom)
+            for j in range(0, window, 1):
+                for k in range(polynom):
+                    first_coeffs[k] += coeffs[k, j] * y[j]
+        elif i == len(x) - half_window - 1:
+            last_coeffs = np.zeros(polynom)
+            for j in range(0, window, 1):
+                for k in range(polynom):
+                    last_coeffs[k] += coeffs[k, j] * y[len(y) - window + j]
+    # Interpolate the result at the left border
+    for i in range(0, half_window, 1):
+        y_smoothed[i] = 0
+        x_i = 1
+        for j in range(0, polynom, 1):
+            y_smoothed[i] += first_coeffs[j] * x_i
+            x_i *= x[i] - x[half_window]
+    # Interpolate the result at the right border
+    for i in range(len(x) - half_window, len(x), 1):
+        y_smoothed[i] = 0
+        x_i = 1
+        for j in range(0, polynom, 1):
+            y_smoothed[i] += last_coeffs[j] * x_i
+            x_i *= x[i] - x[-half_window - 1]
+    return y_smoothed
+
+
+def get_pupil_diameter(XYs, smooth=True):
+    """get mean of pupil diameter in two different ways:
+    d1 = top - bottom, d2 = left - right
+    and in addition assume it's a circle and
+    estimate diameter from other pairs of points
+    Author: Michael Schartner
+    """
+
+    # direct diameters
+    t = XYs['pupil_top_r'].T
+    b = XYs['pupil_bottom_r'].T
+    l = XYs['pupil_left_r'].T
+    r = XYs['pupil_right_r'].T
+
+    ds = []
+    def distance(p1, p2):
+        return ((p1[:, 0] - p2[:, 0]) ** 2 + (p1[:, 1] - p2[:, 1]) ** 2) ** 0.5
+    # get diameter via top-bottom and left-right
+    ds.append(distance(t, b))
+    ds.append(distance(l, r))
+    def dia_via_circle(p1, p2):
+        # only valid for non-crossing edges
+        u = distance(p1, p2)
+        return u * (2 ** 0.5)
+    # estimate diameter via circle assumption
+    for side in [[t, l], [t, r], [b, l], [b, r]]:
+        ds.append(dia_via_circle(side[0], side[1]))
+            
+        
+    diam = np.nanmedian(ds, axis=0)
+    if smooth:
+        return smooth_pupil_diameter(diam)
+    else:
+        return diam
+
+
+if __name__ == "__main__":    
+    eid = '7b074b1a-6576-4380-91e4-ad6cdf06c3a6'
+    video_type = 'left'
+    _, XYs = get_dlc_XYs(eid, video_type)
+    dia = get_pupil_diameter(XYs)
+    
+
+
