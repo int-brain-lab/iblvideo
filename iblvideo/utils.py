6082a732aebe9f7bf092571c6cf908c53501bf7d
juhuntenburg <ju.huntenburg@gmail.com>
2022-01-04 17:36:14 +0000

    Begin to adapt to ibllib task

--- iblvideo/utils.py
+++ iblvideo/utils.py
@@ -5,12 +5,6 @@ from ibllib.exceptions import IblError
 _logger = logging.getLogger('ibllib')
 
 
-# ToDo: remove this here and import from ibblib once merged
-class NvidiaDriverNotReady(IblError):
-    explanation = ('Nvidia driver does not respond. This usually means the GPU is inaccessible and needs to be '
-                   'recovered through a system reboot.')
-
-
 def _run_command(command):
     """
     Run a shell command using subprocess.
@@ -28,12 +22,3 @@ def _run_command(command):
         'process': process,
         'stdout': info.decode(),
         'stderr': error.decode()}
-
-
-def _check_nvidia_status():
-    process = subprocess.Popen('nvidia-smi', shell=True, stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE, executable="/bin/bash")
-    info, error = process.communicate()
-    if process.returncode != 0:
-        raise NvidiaDriverNotReady(f"{error.decode('utf-8')}")
-    _logger.info("nvidia-smi command successful")
