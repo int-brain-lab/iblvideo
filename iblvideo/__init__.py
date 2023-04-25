1d1502796c5a3c6bd519397c9f38db2de25c3fba
juhuntenburg <ju.huntenburg@gmail.com>
2023-04-25 14:03:56 -0400

    rename choiceworld

--- iblvideo/__init__.py
+++ iblvideo/__init__.py
@@ -4,5 +4,5 @@ __version__ = 'iblvideo_2.2.1'  # This is the only place where the version is ha
 import deeplabcut
 
 from iblvideo.run import run_session, run_queue
-from iblvideo.choiceworld import dlc
+from iblvideo.pose_dlc import dlc
 from iblvideo.weights import download_weights
