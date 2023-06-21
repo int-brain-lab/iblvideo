1773b72bfe30f0d0f96db5936f5e89b20f64ad39
juhuntenburg <ju.huntenburg@gmail.com>
2023-06-21 15:32:49 +0200

    bump version

--- iblvideo/__init__.py
+++ iblvideo/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = 'iblvideo_2.2.0'  # This is the only place where the version is hard coded, only adapt here
+__version__ = 'iblvideo_2.2.1'  # This is the only place where the version is hard coded, only adapt here
 
 import deeplabcut
 
