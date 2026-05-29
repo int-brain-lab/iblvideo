3dab2be878b33fada7b1106f3c501edb43b59c9d
Matt Whiteway <themattinthehatt@gmail.com>
2026-05-29 15:51:22 -0400

    delete dead code

--- iblvideo/__init__.py
+++ iblvideo/__init__.py
@@ -5,5 +5,4 @@ __dlc_version__ = 'v2.2'  # versioning for DLC weights/test data
 __lp_version__ = 'v2.1'   # versioning for LP weights/test data
 __la_version__ = 'v1.2'   # versioning for LA weights/test data
 
-from iblvideo.run import run_session, run_queue
 from iblvideo.weights import download_lp_models, download_la_models, download_weights
